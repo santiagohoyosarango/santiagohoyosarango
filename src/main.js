8f56273fe11d311670039d3729f6c1bd85b4bb12
santiagohoyosarango <santiagohoyosarango@gmail.com>
2026-04-10 11:13:23 -0500

    ACTUALIZACION

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './CSS/normalize.css';
+import './CSS/estilos.css';
+import '.CSS/header.css';
+import '.CSS/hero.css';
+import '.CSS/footer.css';
+import '.CSS/variables.css';
+import '.CSS/main.css';
